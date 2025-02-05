1e67e673861b3aff8db164c6804a1bec9977a738
VortexViking <greenmonkey@duck.com>
2025-02-05 09:57:40 -0500

    AUTO MAKE DIRECTORIES

---
+++ command.sh
@@ -0,0 +1,52 @@
+cd ~/Desktop/
+
+mkdir DOWNLOADS && cd DOWNLOADS
+
+mkdir MEDIA 
+mkdir INSTALLERS
+mkdir ZIP
+mkdir CODE
+
+cd MEDIA
+
+mkdir IMAGES
+mkdir VIDEOS
+
+cd IMAGES
+
+mkdir JPG
+mkdir PNG
+mkdir SVG
+mkdir WEBM
+
+cd ..
+
+cd VIDEOS
+
+mkdir MP4
+mkdir MKV
+mkdir GIF
+
+cd ..
+
+cd CODE
+
+mkdir PYTHON
+mkdir BASH
+mkdir MARKDOWN
+
+cd ..
+
+cd INSTALLERS
+
+mkdir DEB
+mkdir RPM
+mkdir FLATPAKREF
+mkdir SNAP
+
+cd ..
+
+cd AUDIO
+
+mkdir MP3
+mkdir WAV
